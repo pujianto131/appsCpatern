1d4e0be45718792a5163c504b78223de39530817
pujianto131 <34307759+pujianto131@users.noreply.github.com>
2017-12-23 21:51:58 +0700

    Add files via upload

---
+++ uas/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int menu[5]={};
+    cout << "===========================\n";
+    cout << "|--- MENU PILIHAN MODUL---|\n";
+    cout << "===========================\n";
+}
