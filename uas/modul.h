2c42d714fc2f6d456b85e0d775b85416443ff7ba
pujianto131 <34307759+pujianto131@users.noreply.github.com>
2017-12-26 21:15:51 +0700

    Update modul.h

--- uas/modul.h
+++ uas/modul.h
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int pola(int x){
+    int i,j;
+    int n=x-1;
+    for (i = 0; i < (2 * n) + 1; i++) {
+            cout<< "\n";
+
+            for (int j = 0; j < (4 * n) + 3; j++) {
+
+
+                if (i <= n) {
+                    if ((j == n - i) || (j == n + i) ) {
+                        cout<<"-";
+                    }
+                    else if((j== n*3-i+2)|| (j==n*3+i+2)){
+                        cout<<"+";
+                    }
+                    else {
+                        cout<<" ";
+                    }
+                }
+                else {
+
+                    if ((j == i - n) || (j == 3*n-i)) {
+                        cout<<"-";
+                    }
+                    else if ((j == i+n+2) || (j == 5*n-i+2) ) {
+                        cout<<"+";
+                    }
+                     else {
+                        cout<<" ";
+                }
+            }
+        }
+    }
+    cout<< endl;
+}
