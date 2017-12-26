a17416be6a3b5128b26db205ff3cc61f1c105446
pujianto131 <34307759+pujianto131@users.noreply.github.com>
2017-12-26 21:15:05 +0700

    Update main.cpp

--- uas/main.cpp
+++ uas/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <string.h>
+#include "modul.h"
 
 using namespace std;
 
 int main()
 {
+    int pilih, nilaiPola;
     int menu[5]={};
     cout << "===========================\n";
     cout << "|--- MENU PILIHAN MODUL---|\n";
-    cout << "===========================\n";
+    cout << "===========================\n\n";
+    cout << "input pilihan : ";
+    cin >> pilih;
+
+    switch (pilih){
+        case 1 :
+            cout << "input nilai\t: ";
+            cin >> nilaiPola;
+            pola(nilaiPola);
+
+    }
 }
